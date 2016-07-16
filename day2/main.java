0cbdaf3d73b3e861b3b074a180462457fc7181e4
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-07-15 20:02:39 -0700

    some notes on very basic things

---
+++ day2/main.java
@@ -0,0 +1,6 @@
+public static void main(String[] args) {
+  // public means any can access
+  // static means it can be run without creating an instance of the main method
+  // void means it doesn't return any value
+  // main is the name of the method that is required
+}
