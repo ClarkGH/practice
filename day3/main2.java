d169cf1d0ac6fba17419220e9603e9530c33ab54
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-07-16 16:59:43 -0700

    different types now variables

--- day3/main2.java
+++ day3/main2.java
@@ -1,6 +1,9 @@
 public class Main2 {
   public static void main(String[] args) {
     String name = "Clark";
+    Int age = 31;
+    double grade = 9.9;
+    char level = "S";
     System.out.println("Printing is easy");
   }
 }
