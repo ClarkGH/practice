01a4d0463143552a863093e49c5167f5ec7183b4
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-07-16 16:53:28 -0700

    simple stuf down

--- day3/main.java
+++
@@ -1,6 +0,0 @@
-public static void main(String[] args) {
-  system.out.println("Shut up meg");
-}
-
-// system.out.println() is a method
-// every statement must end with a colon in javascript
---
+++ day3/note1.java
@@ -0,0 +1,9 @@
+public class note1 {
+  public static void main(String[] args) {
+    System.out.println("Shut up meg");
+  }
+}
+
+
+// system.out.println() is a method
+// every statement must end with a colon in javascript
