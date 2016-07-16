49e808d894bc1208892022e9d5ec69eca29f07a1
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-07-16 16:55:54 -0700

    simple comment notes

--- day3/main2.java
+++ day3/main2.java
@@ -1,5 +1,15 @@
-public class main2 {
+public class Main2 {
   public static void main(String[] args) {
     System.out.println("Printing is easy");
   }
 }
+
+/* 
+Multiline comment
+*/
+
+/**
+ Documentation comment
+*/
+
+//comment
