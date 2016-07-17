9a2cc814de7ddc78fdc731ffbb0ceb575ffe65ae
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-07-17 16:18:12 -0700

    more notes on arrays

--- day4/part4.java
+++ day4/part4.java
@@ -1,4 +1,13 @@
 int[] arr = new int[5];
 arr[2] = 12;
-
 // simple array
+
+System.out.println(arr.length);
+// printing it's length out
+
+int sum = 0;
+for(int i = 0; i < arr.length; i++){
+  sum += arr[i];
+}
+// get the sum off array
+
