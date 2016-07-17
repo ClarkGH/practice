ca99862831411f662a245c855704b214e4a4c01c
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-07-16 17:11:31 -0700

    stuff is gud

---
+++ day3/input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class input {
+  public static void main(String[] args) {
+    Scanner myVar = new Scanner(System.in);
+    System.out.println(myVar.nextLine());
+  }
+}
+
+// imported the scanner class
+// used the scanner class and am outputting user stuff
