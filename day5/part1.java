3fab0484c43c9a8c9ba62cec8493e80b946a6e2d
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-07-18 20:07:09 -0700

    simple method

---
+++ day5/part1.java
@@ -0,0 +1,11 @@
+class Part1 {
+  static void yell() {
+    System.out.println("HOW ARE YOU!");
+  }
+
+  public static void main(String[] args) {
+    yell();
+  }
+}
+
+// This will print out "HOW ARE YOU!"
