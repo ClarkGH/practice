1728f52617279f081cc6d96543ca5d49d4648e7e
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-07-21 22:06:56 -0700

    classes

---
+++ day7/Part2.java
@@ -0,0 +1,9 @@
+// objects
+// Pretend that we're including another file here
+class Part2 {
+  public static void main(String[] args) {
+    Part1 p1 = new Part1();
+    p1.word = "poop";
+    p1.method();
+  }
+}
