4b81883fff3ec1e99a5e9550dc24dd95d4326faa
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-07-22 19:49:48 -0700

    Math expressions in java

---
+++ day8/Part4.java
@@ -0,0 +1,6 @@
+double pow = Math.pow(2, 3);
+int min = Math.min(10, 20);
+int max = Math.max(10, 20);
+int abs = Math.abs(-20);
+double ceiling = Math.ceil(8.23421);
+double floor = Math.floor(8.33);
