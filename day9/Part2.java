78d3c18dd05bceb8008b884d2d948b54e396d51c
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-07-23 09:09:53 -0700

    incrementing method

---
+++ day9/Part2.java
@@ -0,0 +1,10 @@
+public class Part2 {
+  public static void main(String[] args) {
+    Part1 p1 = new Part1();
+    Part1 p2 = new Part1();    
+    Part1 p3 = new Part1();
+    System.out.println(Part1.COUNT);
+  }
+}
+
+// The output is 3
