56491eae37e8ce7a7481ab72bff938503281613e
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-07-26 20:06:17 -0700

    polymorphism

---
+++ day12/Part1.java
@@ -0,0 +1,19 @@
+// Polymorphism
+
+class Part1 {
+  public void eatPudding() {
+    System.out.printlin("nom puddin");
+  }
+}
+
+class Part2 extends Part1 {
+  public void eatPudding() {
+    System.out.println("sluuuuurp");
+  }
+}
+
+class Part3 extends Part1 {
+  public void eatPudding() {
+    System.out.println("numnumnumnumnum")
+  }
+}
