0e49920869d63c09554673196b430c03dbdf7b0a
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-07-27 18:40:48 -0700

    I am mad for no reason overriding things man I committed a stuff and learns

---
+++ day13/Part1.java
@@ -0,0 +1,11 @@
+class Part1 {
+  public void rickSound() {
+    System.out.println("Wubba lubba dub dub");
+  }
+}
+
+class Wat extends Part1{
+  public void rickSound() {
+    System.out.println("I am saucy.");
+  }
+}
