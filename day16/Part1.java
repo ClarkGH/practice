9828f7abb78352c8145b2b5cef646094b1cde074
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-07-30 14:29:45 -0700

    more anon

--- day16/Part1.java
+++ day16/Part1.java
@@ -1,10 +1,17 @@
 // anonymous classes
 
+class Part1 {
+  public void go(){
+    System.out.println("here we gooo");
+  }
+}
+
 public static void main(String[] args){
-  Part1 p = new Part1() {
+  Part1 p1 = new Part1() {
     @Override public void go() {
       System.out.println("ohhhh yeah");
     }
   };
-  a.go();
+  Part1 p2 = new Part1();
+  p2.go();
 }
