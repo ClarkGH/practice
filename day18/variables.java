d76fbd831f555e6ed1abba04b86bbde06a16942d
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-06 19:06:57 -0700

    Easy peez

--- day18/variables.java
+++ day18/variables.java
@@ -1,6 +1,10 @@
 public class Variables {
   public static void main(String[] args) {
     int age = 31;
-    
+    boolean food = true;
+    double decimalllls = 2.1;
+    String greeting = "hi";
+
+    System.out.println(greeting + " bob");
   }
 }
