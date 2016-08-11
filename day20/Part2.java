cb0a13d1c18a40e929e227f983366f6c3ccb0fd7
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-10 20:51:54 -0700

    Tina Turner class

---
+++ day20/Part2.java
@@ -0,0 +1,16 @@
+// classes and objects
+
+public class Part2 {
+  public static void main (String[] args) {
+
+    class PrivateDancer {
+
+      String dance = "";
+    }
+
+    PrivateDancer tinaTurner = new PrivateDancer();
+
+    tinaTurner.dance = "Dancing for money";
+
+  }
+}
