039de4b8ab3efcb64b81e2db191672ad6b442ee1
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-10 20:54:59 -0700

    Forks

--- day20/Part2.java
+++ day20/Part2.java
@@ -6,11 +6,19 @@ public class Part2 {
     class PrivateDancer {
 
       String dance = "";
+
+      public boolean theBodyGuard() {
+        if (1 < 2) {
+          return true;
+        } else {
+          return false;
+        }
+      }
     }
 
     PrivateDancer tinaTurner = new PrivateDancer();
 
     tinaTurner.dance = "Dancing for money";
-
+    System.out.println(PrivateDancer.theBodyGuard());
   }
 }
