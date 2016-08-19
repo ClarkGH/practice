ab377e48e307b9c4c0af1ad2eaf2f37bfa1caf58
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-19 08:48:58 -0700

    Wubba lubba dub dub

--- day21/Part1.java
+++ day21/Part1.java
@@ -1,5 +1,7 @@
 class Part1 {
   static void main(String[] args){
-    System.out.println("The mayor loves cheeese");
+    if(1 < 2) {
+      System.out.println("The mayor loves cheeese");      
+    }
   }
 }
