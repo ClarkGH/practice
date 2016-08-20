b28e4a21fb948d3c26f10f10c3d1ac3c329aa1de
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-19 22:57:52 -0700

    k

--- day21/Part1.java
+++ day21/Part1.java
@@ -2,6 +2,9 @@ class Part1 {
   static void main(String[] args){
     if(1 < 2) {
       System.out.println("The mayor loves cheeese");      
+    }else{
+      System.out.println("The mayor hates cheeese");      
+
     }
   }
 }
